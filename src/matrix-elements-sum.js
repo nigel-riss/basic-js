5f7a344aed910c5f576480c0e161435ee647205a
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2023-04-15 04:22:34 +0200

    feat: add getMatrixElementsSum solution

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -31,13 +31,6 @@ const getMatrixElementsSum = matrix => {
 }
 
 
-console.log(getMatrixElementsSum([
-  [0, 1, 1, 2],
-  [0, 5, 0, 0],
-  [2, 0, 3, 3],
-]))
-
-
 module.exports = {
   getMatrixElementsSum
 };
