74a7d60320ca90e7266033a90b656537e7509d30
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2023-04-16 02:53:53 +0200

    feat(WIP): add VigenereCipheringMachine solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
@@ -19,26 +17,84 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+const ENCRYPT = 1
+const DECRYPT = -1
+const ALPHABET_START = 65
+const ALPHABET_LENGTH = 26
+
 class VigenereCipheringMachine {
   constructor(isDirect) {
     this.isDirect = isDirect
   }
 
   _checkArgs(text, key) {
-    // throw new NotImplementedError('Not implemented');
     if (!text || !key) {
       throw new Error(`Incorrect arguments!`)
     }
   }
 
-  encrypt(text, key) {
+  _getFullKey(key, textLength) {
+    const keyRepeat = Math.ceil(textLength / key.length)
+    return key
+      .repeat(keyRepeat)
+      .toUpperCase()
+      .split(``)
+  }
+
+  _getCryptedChar(char, direction = ENCRYPT) {
+    const charCode = char.charCodeAt(0)
+
+    if (
+      charCode < ALPHABET_START 
+      || charCode > ALPHABET_START + ALPHABET_LENGTH - 1
+    ) { return char }
+
+    const keyCode = this.fullKey.shift().charCodeAt(0)
+
+    let shift = (keyCode - ALPHABET_START) * direction;
+    if (direction === DECRYPT) { shift += ALPHABET_LENGTH }
+    const newCharCode = (charCode - ALPHABET_START + shift)
+      % ALPHABET_LENGTH
+      + ALPHABET_START
+
+    return String.fromCharCode(newCharCode)
+  }
+
+  _processText(text, key, direction = ENCRYPT) {
     this._checkArgs(text, key)
+
+    this.fullKey = this._getFullKey(key, text.length)
+    text = text.toUpperCase()
+
+    let cryptedText = ``
+    for (let i = 0; i < text.length; i++) {
+      cryptedText += this._getCryptedChar(text[i], direction)
+    }
+
+    return cryptedText
+  }
+
+  encrypt(text, key) {
+    return this._processText(text, key, ENCRYPT)
   }
 
   decrypt(text, key) {
-    this._checkArgs(text, key)
+    return this._processText(text, key, DECRYPT)
   }
 }
+
+
+
+const directMachine = new VigenereCipheringMachine();
+const reverseMachine = new VigenereCipheringMachine(false);
+
+
+console.log(directMachine.decrypt(`bcdef bcdef`, `b`))
+console.log(directMachine.encrypt('LEARN frontend DEVELOPMENT :)', 'js'), 'UWJJW XAGWLNFM VNNNDXHVWWL :)');
+console.log(directMachine.decrypt('ICWWQAM KECEIK JVZZT EADGG!', 'rollingscopes'), 'ROLLING SCOPES SHOOL RULES!');
+console.log(directMachine.decrypt('TRVVFB VT JSUIFMYL!', 'learning'), 'INVEST IN YOURSELF!');
+
+
 
 module.exports = {
   VigenereCipheringMachine
